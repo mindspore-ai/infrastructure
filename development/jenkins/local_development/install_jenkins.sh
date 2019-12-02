e66596819c89828275da2b20671c0590a598e5e6
TommyLike <hu.husheng@huawei.com>
2019-12-02 15:56:45 +0800

    Update jenkins yaml file

--- development/jenkins/local_development/install_jenkins.sh
+++ development/jenkins/local_development/install_jenkins.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export CURRENT_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-helm template ${CURRENT_ROOT}/../jenkins -f ${CURRENT_ROOT}/../jenkins/values.yaml -f ${CURRENT_ROOT}/../jenkins/local_override.yaml --name mindspore > jenkins.yaml
+export KUBECONFIG="$(kind get kubeconfig-path --name="development")"
+helm template ${CURRENT_ROOT}/../jenkins -f ${CURRENT_ROOT}/../jenkins/values.yaml -f ${CURRENT_ROOT}/../jenkins/local_override.yaml --name mindspore | kubectl apply -f -
