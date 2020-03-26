59014ececd25d0665516e519892e8ac1a2b460c8
TommyLike <hu.husheng@huawei.com>
2020-03-26 16:40:35 +0800

    Remove debug and unnecessary files

--- development/jenkins/local_development/install_jenkins.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export CURRENT_ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export KUBECONFIG="$(kind get kubeconfig-path --name="development")"
-helm template ${CURRENT_ROOT}/../jenkins -f ${CURRENT_ROOT}/../jenkins/values.yaml -f ${CURRENT_ROOT}/../jenkins/local_override.yaml --name mindspore | kubectl apply -f -
