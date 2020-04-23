3313c9a7112419abf73dd838939666628a624034
TommyLike <tommylikehu@gmail.com>
2020-04-23 09:20:16 +0800

    Update mindspore update pr groovy script by new Gitee API

--- development/jenkins/utils/patchPullRequest.groovy
+++ development/jenkins/utils/patchPullRequest.groovy
@@ -1,53 +1,42 @@
-import groovy.json.JsonSlurper
 def addOrRemoveLables(String OWNER, String PROJECT, String PR_NUMBER, String ACCESS_TOKEN, ArrayList ADD_LABELS, ArrayList REMOVE_LABLES) {
 	if (!(OWNER?.trim() && PROJECT?.trim() && PR_NUMBER?.trim() && ACCESS_TOKEN?.trim()))  {
     	throw new Exception("any of 'OWNER', 'PROJECT', 'PR_NUMBER', 'ACCESS_TOKEN' should not be empty")
 	}
-	//获取PR的详细信息
-	def requestUrl = sprintf("https://gitee.com/api/v5/repos/%s/%s/pulls/%s?access_token=%s", OWNER, PROJECT, PR_NUMBER, ACCESS_TOKEN)
-    def response = httpRequest quiet: true, consoleLogResponseBody: false,
-    						   contentType: 'APPLICATION_JSON',
-    						   customHeaders: [[name: "User-Agent", value: "MindSpore"]],
-    						   httpMode: 'GET',
-    						   url: requestUrl,
-    						   ignoreSslErrors: true,
-    						   validResponseCodes: "200";
-    //解析PR下面现有的标签
-    def pullRequest = new JsonSlurper().parseText(response.content)
-    def prNames = pullRequest.labels.collect{ it.name }
-    println "pr existing labels: $prNames"
-
-    //添加或者移除PR标签
+  try {
+    println "going to update pr $OWNER, $PROJECT, $PR_NUMBER,$ADD_LABELS,$REMOVE_LABLES"
+    //删除PR的标签
     REMOVE_LABLES.eachWithIndex { v, index ->
-     	prNames.remove(v)
-    }
-    ADD_LABELS.eachWithIndex { v, index ->
-	    if (!prNames.contains(v)) {
-	    	prNames.add(v)
-	     }
+      def requestUrl = sprintf("https://gitee.com/api/v5/repos/%s/%s/pulls/%s/labels/%s?access_token=%s", OWNER, PROJECT, PR_NUMBER, v, ACCESS_TOKEN)
+      println requestUrl
+      def response = httpRequest quiet: true, consoleLogResponseBody: false,
+                     contentType: 'APPLICATION_JSON',
+                     customHeaders: [[name: "User-Agent", value: "MindSpore"]],
+                     httpMode: 'DELETE',
+                     url: requestUrl,
+                     ignoreSslErrors: true,
+                     validResponseCodes: "204,404";
+      //unset response or will raise exception
+      response = null
     }
 
-    //unset response or will raise exception
-    pullRequest = null
-    response = null
-
-    //更新PR的标签列表
-    def updateUrl = sprintf("https://gitee.com/api/v5/repos/%s/%s/pulls/%s", OWNER, PROJECT, PR_NUMBER)
-    def prNameStr = prNames.join(',')
+    //添加新的标签
+    def requestUrl = sprintf("https://gitee.com/api/v5/repos/%s/%s/pulls/%s/labels?access_token=%s", OWNER, PROJECT, PR_NUMBER, ACCESS_TOKEN)
+    def prNameStr = ADD_LABELS.join(',')
     def updatedLabels = """
-    	{"access_token": "$ACCESS_TOKEN", "labels": "$prNameStr"}
-    """
-    println "going to update pr labels: $prNameStr"
-    response = httpRequest quiet: true,
-                           acceptType: 'APPLICATION_JSON',
-                           consoleLogResponseBody: false,
-                           contentType: 'APPLICATION_JSON',
-                           requestBody: updatedLabels,
-                           customHeaders: [[name: "User-Agent", value: "MindSpore"]],
-                           httpMode: 'PATCH',
-                           url: updateUrl,
-                           validResponseCodes: "200";
-
+        ["$prNameStr"]
+      """
+    println requestUrl
+    def response = httpRequest quiet: true, consoleLogResponseBody: false,
+                   contentType: 'APPLICATION_JSON',
+                   customHeaders: [[name: "User-Agent", value: "MindSpore"]],
+                   httpMode: 'POST',
+                   requestBody: updatedLabels,
+                   url: requestUrl,
+                   ignoreSslErrors: true,
+                   validResponseCodes: "201";
     //unset response or will raise exception
     response = null
+  }catch(Exception ex){
+    println "failed to update pr $OWNER, $PROJECT, $PR_NUMBER,$ADD_LABELS,$REMOVE_LABLES, $ex"
+  }
 }
